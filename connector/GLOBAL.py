778ae64eb5a0633ec11bf0f5c7dd022bf0614177
Evgeniy Korneechev <ekorneechev@altlinux.org>
2016-08-26 08:08:23 +0300

    Added mode 'Kiosk'

--- connector/GLOBAL.py
+++ connector/GLOBAL.py
@@ -10,19 +10,47 @@ HOMEFOLDER = os.getenv('HOME')
 WORKFOLDER = HOMEFOLDER + '/.connector/'
 
 #Установки по умолчанию для параметров программы (какие приложения использовать)
-DEFAULT = dict(RDP = 1, VNC = 0, TAB = '0') 
+DEFAULT = dict(RDP = 1, VNC = 0, TAB = '0', KIOSK = '0') 
 
 #Версия приложения
-VERSION = "1.3.23"
+VERSION = "1.4.0-beta"
 
 #Исходные данные для ярлыка подключения
-DESKTOP_INFO ="""#!/usr/bin/env xdg-open
+DESKTOP_INFO = """#!/usr/bin/env xdg-open
 [Desktop Entry]
 Version=1.0
 Type=Application
 Terminal=false
 Icon=/usr/share/connector/data/emblem.png
 """
+
+#Режим киоска
+KIOSK_MARCO = """marco &
+while true
+do
+connector
+done
+"""
+
+KIOSK_OPENBOX = """openbox &
+while true
+do
+connector
+done
+"""
+
+KIOSK_X = """#!/usr/bin/env xdg-open
+[Desktop Entry]
+Version=1.0
+Type=Application
+Terminal=false
+Icon[ru_RU]=start
+Name[ru_RU]=Ctor_kiosk
+Exec=startx
+Name=Ctor_kiosk
+Icon=start
+"""
+
 
 #Запускаемый файл приложения
 EXEC = "/usr/bin/connector "
--- connector/properties.py
+++ connector/properties.py
@@ -70,15 +70,18 @@ class Properties(Gtk.Window):
         self.changeRdpRem = builder.get_object("radio_RDP_remmina")
         self.changeVncRem = builder.get_object("radio_VNC_remmina")
         self.statusbar = builder.get_object("statusbar")
+        self.check_kiosk = builder.get_object("check_kiosk")
         self.combo_tabs = builder.get_object("combo_tabs")
         changeRdpFree = builder.get_object("radio_RDP_freeRDP")
         changeVncView = builder.get_object("radio_VNC_viewer")
-        self.program = loadFromFile('default.conf')
-        if self.program['RDP']:
+        self.defaultConf = loadFromFile('default.conf')
+        if self.defaultConf['RDP']:
             changeRdpFree.set_active(True)
-        if self.program['VNC']:
+        if self.defaultConf['VNC']:
             changeVncView.set_active(True)
-        try: self.combo_tabs.set_active_id(self.program['TAB'])
+        if self.defaultConf['KIOSK']:
+            self.check_kiosk.set_active(True)
+        try: self.combo_tabs.set_active_id(self.defaultConf['TAB'])
         except KeyError: self.combo_tabs.set_active_id('0')
         self.add(box)        
         self.connect("delete-event", self.onClose)
@@ -94,16 +97,21 @@ class Properties(Gtk.Window):
     def onSave (self, *args):
         """Сохранение настроек программ по умолчанию"""
         if self.changeRdpRem.get_active():
-            self.program['RDP'] = 0
-        else: self.program['RDP'] = 1
+            self.defaultConf['RDP'] = 0
+        else: self.defaultConf['RDP'] = 1
         if self.changeVncRem.get_active():
-            self.program['VNC'] = 0
-        else: self.program['VNC'] = 1
-        self.program['TAB'] = self.combo_tabs.get_active_id()
-        saveInFile('default.conf',self.program)
+            self.defaultConf['VNC'] = 0
+        else: self.defaultConf['VNC'] = 1
+        if self.check_kiosk.get_active():
+            self.enableKiosk()
+        else: 
+            self.defaultConf['KIOSK'] = 0
+            self.disableKiosk()
+        self.defaultConf['TAB'] = self.combo_tabs.get_active_id()
+        saveInFile('default.conf',self.defaultConf)
         gui.viewStatus(self.statusbar, "Настройки сохранены в файле default.conf...")
 
-    def clearFile(self, filename, title, message):
+    def clearFile (self, filename, title, message):
         """Функция для очисти БД серверов или списка подключений"""
         dialog = Gtk.MessageDialog(self, 0, Gtk.MessageType.WARNING, Gtk.ButtonsType.OK_CANCEL, title)
         dialog.format_secondary_text(message)
@@ -114,13 +122,52 @@ class Properties(Gtk.Window):
             gui.viewStatus(self.statusbar, "Выполнено, изменения вступят в силу после перезапуска...")
         dialog.destroy()    
 
-    def onClearServers(self, widget):
+    def onClearServers (self, widget):
         self.clearFile("servers.db", "Подтвердите очистку БД",
                       "Вы потеряете всю историю посещений!!!")
 
-    def onClearConnects(self, widget):
+    def onClearConnects (self, widget):
         self.clearFile("connections.db", "Подтвердите очистку списка подключений",
                       "Все Ваши сохраненные подключения удалятся!!!")
+
+    def enableKiosk (self):
+        """Включение режима киоска, создание необходимых для этого файлов"""
+        def saveXsession(wm):                   
+            xsession = open (HOMEFOLDER + "/.xsession", "w")
+            xsession.write(wm)
+            xsession.close()
+            return True
+        existsXs = False #будет ли записан файл
+        marco = int(os.popen('which marco > /dev/null 2> /dev/null; echo $?').read()) #0 если установлено.
+        if marco == 0: existsXs = saveXsession(KIOSK_MARCO)
+        else:
+            openbox = int(os.popen('which openbox > /dev/null 2> /dev/null; echo $?').read())
+            if openbox == 0: existsXs = saveXsession(KIOSK_OPENBOX)
+            else:
+                dialog = Gtk.MessageDialog(self, 0, Gtk.MessageType.ERROR, Gtk.ButtonsType.OK, "Оконный менеджер не найден!")
+                dialog.format_secondary_text("Для работы в режиме киска необходимо\nустановить один из оконных менеджеров:\n- marco\n- openbox")
+                response = dialog.run()
+                dialog.destroy()
+                self.check_kiosk.set_active(False)#автоматическое снятие чекбокса
+        if existsXs:
+            try: os.chmod(HOMEFOLDER+"/.xsession", 0o766) #chmod +x            
+            except FileNotFoundError: pass
+            autostartFile = HOMEFOLDER + "/.config/autostart/Ctor_kiosk.desktop"
+            autostart = open (autostartFile, "w")
+            autostart.write(KIOSK_X)
+            autostart.close()
+            os.chmod(autostartFile, 0o766)
+            dialog = Gtk.MessageDialog(self, 0, Gtk.MessageType.INFO, Gtk.ButtonsType.OK, 'Программа настроена на режим "Киоск"')
+            dialog.format_secondary_text("При следующем входе в сеанс пользователя запуститься только Connector.\nCtrl+Alt+F1 - откроется рабочий стол")
+            response = dialog.run()
+            dialog.destroy()
+            self.defaultConf['KIOSK'] = 1
+
+    def disableKiosk (self):
+        try:
+            os.remove(HOMEFOLDER + "/.config/autostart/Ctor_kiosk.desktop")
+            os.remove(HOMEFOLDER + "/.xsession")
+        except: pass
 
 if __name__ == '__main__':
     pass
